25499f2d5d2d1406e6b953245b5cbef834a3f078
silkylaroux <31264226+silkylaroux@users.noreply.github.com>
2018-02-17 22:53:03 -0700

    working on it
    stuff

--- A2/notebookcode.py
+++ A2/notebookcode.py
@@ -1,49 +1,7 @@
 
-# coding: utf-8
-
-# $\newcommand{\xv}{\mathbf{x}}
-# \newcommand{\Xv}{\mathbf{X}}
-# \newcommand{\yv}{\mathbf{y}}
-# \newcommand{\zv}{\mathbf{z}}
-# \newcommand{\av}{\mathbf{a}}
-# \newcommand{\Wv}{\mathbf{W}}
-# \newcommand{\wv}{\mathbf{w}}
-# \newcommand{\tv}{\mathbf{t}}
-# \newcommand{\Tv}{\mathbf{T}}
-# \newcommand{\muv}{\boldsymbol{\mu}}
-# \newcommand{\sigmav}{\boldsymbol{\sigma}}
-# \newcommand{\phiv}{\boldsymbol{\phi}}
-# \newcommand{\Phiv}{\boldsymbol{\Phi}}
-# \newcommand{\Sigmav}{\boldsymbol{\Sigma}}
-# \newcommand{\Lambdav}{\boldsymbol{\Lambda}}
-# \newcommand{\half}{\frac{1}{2}}
-# \newcommand{\argmax}[1]{\underset{#1}{\operatorname{argmax}}}
-# \newcommand{\argmin}[1]{\underset{#1}{\operatorname{argmin}}}$
-
-# # Assignment 2: Neural Network Regression
-
-# *Type your name here and rewrite all of the following sections.  Add more sections to present your code, results, and discussions.*
-
-# ## Overview
-
-# The goal of this assignment is to learn about object-oriented programming in python and to gain some experience in comparing different sized neural networks when applied to a data set.
-# 
-# Starting with the ```NeuralNetwork``` class from the lecture notes, you will create one new version of that class, apply it to a data set, and discuss the results.
-
-# ## Required Code
-
-# Start with the ```NeuralNetwork``` class defined in lecture notes 09. Put that class definition as written into *neuralnetworks.py* into your current directory.  Also place *mlutilities.py* from lecture notes 09 in your current directory. If this is done correctly, then the following code should run and produce results similar to what is shown here.
-
-# In[8]:
-
-
 import numpy as np
 import matplotlib.pyplot as plt
-get_ipython().magic('matplotlib inline')
-
-
-# In[27]:
-
+%matplotlib inline
 
 import neuralnetworks as nn
 
@@ -54,10 +12,6 @@ nnet = nn.NeuralNetwork(1, [10], 1)
 nnet.train(X, T, 100, verbose=True)
 nnet
 
-
-# In[14]:
-
-
 plt.figure(figsize=(8, 12))
 plt.subplot(3, 1, 1)
 plt.plot(nnet.getErrors())
@@ -68,47 +22,17 @@ plt.plot(X, nnet.use(X), 'o-', label='Predicted')
 
 plt.subplot(3, 1, 3)
 nnet.draw()
-
-
-# Now let's extract the parts of the neural network code that refer to the activation function and its derivative into two new methods.  Copy your file *neuralnetworks.py* into a new file named *neuralnetworks2.py*.  Modify the code in *neuralnetworksA2.py* by adding these two methods to the ```NeuralNetwork``` class:
-# 
-#     def activation(self, weighted_sum):
-#         return np.tanh(weighted_sum)
-#         
-#     def activationDerivative(self, activation_value):
-#         return 1 - activation_value * activation_value
-#         
-# Now replace the code in the appropriate places in the ```NeuralNetwork``` class so that ```np.tanh``` is replaced with a call to the ```self.activation``` method and its derivative is replaced by calls to ```self.activationDerivative```.
-
-# In[32]:
-
 
 import neuralnetworksA2 as nn2
 
 nnet = nn2.NeuralNetwork(1, [10], 1)
 
-
-# In[33]:
-
-
 [nnet.activation(s) for s in [-2, -0.5, 0, 0.5, 2]]
-
-
-# In[34]:
-
 
 [nnet.activationDerivative(nnet.activation(s)) for s in [-2, -0.5, 0, 0.5, 2]]
 
-
-# In[35]:
-
-
 nnet.train(X, T, 100, verbose=True)
 nnet
-
-
-# In[36]:
-
 
 plt.figure(figsize=(8, 12))
 plt.subplot(3, 1, 1)
@@ -120,16 +44,6 @@ plt.plot(X, nnet.use(X), 'o-', label='Predicted')
 
 plt.subplot(3, 1, 3)
 nnet.draw()
-
-
-# ## Neural Network Performance with Different Hidden Layer Structures and Numbers of Training Iterations
-
-# ### Example with Toy Data
-
-# Using your new ```NeuralNetwork``` class, you can compare the error obtained on a given data set by looping over various hidden layer structures.  Here is an example using the simple toy data from above.
-
-# In[28]:
-
 
 import random
 
@@ -142,22 +56,10 @@ testRows = rows[nTrain:]
 Xtrain, Ttrain = X[trainRows, :], T[trainRows, :]
 Xtest, Ttest = X[testRows, :], T[testRows, :]
 
-
-# In[29]:
-
-
 Xtrain.shape, Ttrain.shape, Xtest.shape, Ttest.shape
-
-
-# In[30]:
-
 
 def rmse(A, B):
     return np.sqrt(np.mean((A - B)**2))
-
-
-# In[31]:
-
 
 import pandas as pd
 
@@ -177,13 +79,17 @@ plt.legend(('Train RMSE', 'Test RMSE'))
 plt.xticks(range(errors.shape[0]), hiddens, rotation=30, horizontalalignment='right')
 plt.grid(True)
 
-
-# For this data (and the random shuffling of the data used when this notebook was run), `[10, 10, 10, 10]` produced the lowest test error.  
-# 
-# Now, using the best hidden layer structure found, write the code that varies the number of training iterations. The results you get will be different from the ones shown below.
-
-# In[35]:
-
+import pandas as pd
+errors = []
+hiddens = [0] + [[nu] * nl for nu in [1, 5, 10, 20, 50] for nl in [1, 2, 3, 4, 5]]
+print('hiddens =', hiddens)
+nIterationsList = []
+for hids in hiddens: 
+    nnet = nn.NeuralNetwork(Xtrain.shape[1], hids, Ttrain.shape[1])
+    nnet.train(Xtrain, Ttrain, 500)
+    nIterationsList.append([hids])
+    errors.append([hids, rmse(Ttrain, nnet.use(Xtrain)), rmse(Ttest, nnet.use(Xtest))])
+errors = pd.DataFrame(errors)
 
 #  ...  insert code here using the code in the previous code block as a guide ...
 
@@ -192,46 +98,7 @@ print(errors)
 plt.figure(figsize=(10, 10))
 plt.plot(errors.values[:, 1:], 'o-')
 plt.legend(('Train RMSE', 'Test RMSE'))
-plt.xticks(range(errors.shape[0]), nIterationsList) # , rotation=30, horizontalalignment='right')
+plt.xticks(range(errors.shape[0]), nIterationsList, rotation=30, horizontalalignment='right')
 plt.grid(True)
 
-
-# ### Experiments wtih Automobile Data
-
-# Now, repeat the above steps with the automobile mpg data we have used before.  Set it up to use 
-# 
-#   * cylinders,
-#   * displacement,
-#   * weight,
-#   * acceleration,
-#   * year, and
-#   * origin
-#   
-# as input variables, and
-# 
-#   * mpg
-#   * horsepower
-#   
-# as output variables.
-
-# Include the code, output, and graphs like the above examples. Discuss the results.  Investigate and discuss how much the best hidden layer structure and number of training iterations vary when you repeat the runs.
-
-# ## Grading and Check-in
-
-# Your notebook will be run and graded automatically. Test this grading process by first downloading [A2grader.tar](http://www.cs.colostate.edu/~anderson/cs445/notebooks/A2grader.tar) and extract `A2grader.py` from it. Run the code in the following cell to demonstrate an example grading session. You should see a perfect execution score of  60 / 60 if your functions and class are defined correctly. The remaining 40 points will be based on the results you obtain from the comparisons of hidden layer structures and numbers of training iterations on the automobile data.
-# 
-# For the grading script to run correctly, you must first name this notebook as `Lastname-A2.ipynb` with `Lastname` being your last name, and then save this notebook.  Your working director must also contain `neuralnetworksA2.py` and `mlutilities.py` from lecture notes.
-# 
-# Combine your notebook and `neuralnetworkA2.py` into one zip file or tar file.  Name your tar file `Lastname-A2.tar` or your zip file `Lastname-A2.zip`.  Check in your tar or zip file using the `Assignment 2` link in Canvas.
-# 
-# A different, but similar, grading script will be used to grade your checked-in notebook. It will include other tests.
-
-# In[37]:
-
-
-get_ipython().magic('run -i A2grader.py')
-
-
-# ## Extra Credit
-
-# Repeat the comparisons of hidden layer structures and numbers of training iterations on a second data set from the [UCI Machine Learning Repository](https://archive.ics.uci.edu/ml).
+%run -i A2grader.py
